72c03e8e30ea2112cc291151343d21bad44bea17
Unusual-Intellectual <leshalitova@gmail.com>
2021-07-15 16:36:38 +0300

    without excess

--- unit_10_jdbc/src/main/java/solutions/MinDistances.java
+++ unit_10_jdbc/src/main/java/solutions/MinDistances.java
@@ -27,28 +27,4 @@ public class MinDistances {
         }
         return distance;
     }
-
-//    private int[] getResult(List<String> input, int[][] distances){
-//        int[] rows = new int[100];
-//        int counter = 0;
-//
-//        for(String in : input){
-//            String[] distance = in.split(" ");
-//            if(Character.isLetter(distance[0].charAt(0)) && distance.length == 2){
-//                int firstCityIndex = cities.get(distance[0]);
-//                int secondCityIndex = cities.get(distance[1]);
-//                if(distances[firstCityIndex-1][secondCityIndex-1] == MAX)
-//                    rows[counter] = -1;
-//                else
-//                    rows[counter] = distances[firstCityIndex-1][secondCityIndex-1];
-//                counter++;
-//            }
-//        }
-//
-//        int[] result = new int[counter];
-//        for (int i = 0; i < counter; i++) {
-//            result[i] = rows[i];
-//        }
-//        return result;
-//    }
 }
